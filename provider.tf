61cc3fc372dda763fd4820f528c8ceacc55df027
mawuena <christopher.askademia@gmail.com>
2025-01-07 19:18:36 -0600

    update role

--- provider.tf
+++ provider.tf
@@ -11,7 +11,7 @@ terraform {
 provider "aws" {
   region = "us-east-1"
   assume_role {
-    role_arn = "arn:aws:iam::058264383077:role/adminn"
+    role_arn = "arn:aws:iam::058264383077:instance-profile/aws-jenkins"
   }
 
 }
